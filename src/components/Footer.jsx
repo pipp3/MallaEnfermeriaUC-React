12657c3a01844e7a6f47b7c04054924530d3dd4e
Felipe Muñoz <felipe@MacBook-Air-de-Felipe.local>
2025-03-27 22:24:14 -0300

    Cambios pequeños en footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,17 @@ const Footer = () => {
   return (
     <footer className="flex justify-center font-mono text-sm text-sky-600 bg-pink-100 pb-2 pt-2">
       <p>
-        Created by <span className="font-bold cursor-pointer">pipp3</span>
+        Created by{" "}
+        <a
+          href="https://portafolio-felipe-munozve.netlify.app"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="font-bold cursor-pointer hover:text-sky-800"
+        >
+          pipp3
+        </a>
       </p>
-      <img src="/github.svg" alt="github logo" className="w-5" />
+      <img src="/portafolio.svg" alt="github logo" className="w-5 ml-1" />
     </footer>
   );
 };
